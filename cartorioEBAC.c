9eaa0539f75f4dc8352d54b2f699e07512f39d11
AnnaMont-00 <Annamntr00@gmail.com>
2024-07-28 17:43:52 -0300

    opção de saida
    incluso opção de saida para o usuario

--- cartorioEBAC.c
+++ cartorioEBAC.c
@@ -129,6 +129,7 @@ int main ()
 	printf("\t1 - Registrar nomes\n");
 	printf("\t2 - Consultar nomes\n");
 	printf("\t3 - Deletar nomes\n");
+	printf("\t4 - sair do sistema\n ");
 	printf("Opção");// Fim do menu 
 	
 	scanf ("%d", &opcao );// Armazenando a escolha do usuario 
@@ -148,6 +149,12 @@ int main ()
 	   
 	   case 3:
 	   Deletar();
+	   break;
+	   
+	   case 4:
+	   printf ("Obrigado por utilizar o sistema!");
+	   return 0;
+	   system("pause");
 	   break;
 	 
 	   default:
